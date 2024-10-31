68889b623b3d8f4f2dff31e5e993fe56fc2acf8b
Hussein Ghaly <hmghaly@users.noreply.github.com>
2024-10-30 22:23:03 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1011,12 +1011,12 @@ function get_dist(obj1,obj2){
 // }
 
 //https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
-function parseJwt (token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+// function parseJwt (token) {
+//     var base64Url = token.split('.')[1];
+//     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+//     var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+//         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+//     }).join(''));
 
-    return JSON.parse(jsonPayload);
-}
+//     return JSON.parse(jsonPayload);
+// }
